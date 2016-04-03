10b6daa70fc7d00c7fec3c3c2967b888b84b951a
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-04-02 22:21:47 -0500

    Use minitest reporters for RED/GREEN test.  Use setup method for static page tests to supply title dynamically.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Critter"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Critter"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Critter"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Critter"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
