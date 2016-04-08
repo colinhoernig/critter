12b85033e7370a645300aaa3c39a02938233b35a
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-04-07 22:26:16 -0500

    Build out layout, split into partials.  Add site layout integration test.  Add bootstrap-sass.  Configure named routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
