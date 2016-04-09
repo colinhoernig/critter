68f4831cfccc7b5b50286f3986d2cca9ebcf7bd6
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-04-08 21:31:17 -0500

    Set up secure passwords.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
